5e235f878ea47b96810f628cd0683b9109df84e3
Mashudu Molema <mrkaymp@gmail.com>
2025-08-05 15:26:07 +0200

    Setup project with React Hooks.

---
+++ alx-project-0x07/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index: React.FC = () => {
+  return (
+    <div className="text-gray-300 h-screen flex  justify-center items-center bg-gray-500">Landing page...</div>
+  )
+}
+
+export default index
+
+
+
+
